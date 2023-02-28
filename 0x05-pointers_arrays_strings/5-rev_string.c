9d0a96ef09393935abffec379e0e54d7e23e2d3f
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-02-28 05:46:02 -0800

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * rev_string -reverses a string
- * @s: string
- * Return: void
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
  */
+
 void rev_string(char *s)
 {
-	int len = 0, index = 0;
-	char tmp;
+	char rev = s[0];
+	int counter = 0;
+	int i;
 
-	while (s[index++])
-	len++;
-
-	for (index = len - 1; index >= len / 2; index--);
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
 	{
-	tmp = s[index];
-	s[index] = s[len - index - 1];
-	s[len - index - 1] = tmp;
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
 }
