6c334bee63cbf60c894e8e608f50b5dde6648961
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-03-11 05:45:04 -0800

    name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints no of arguments passed to the program
+ * @argc: no of arguments
+ * @argv: array of argument
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc,char * argv[])
+{
+	printf("argc = %d\n", argc - 1);
+		return (0);
+
