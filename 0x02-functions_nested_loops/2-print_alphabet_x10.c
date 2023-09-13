4a770d81cef18137b9d183abdec97c0b5406abdd
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-09-13 11:57:30 -0700

    9

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 	int count;
 
 	count = 0;
-	while (count <= 10)
+	while (count <= 9)
 	{
-		for (letter = 'a'; letter < 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		count++;
 		_putchar('\n');
