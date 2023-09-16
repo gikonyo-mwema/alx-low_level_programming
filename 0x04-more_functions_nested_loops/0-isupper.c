12d64e27610f7ddb4646dd3d59a74d4cf17969e9
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-09-15 23:49:18 -0700

    check if upppercase + REadme + main.h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 (uppercase) otherwise 0
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 99);
+}
