6237830bf51a09f9d3341fbe44ae819b3dbf95bb
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-09-16 11:47:10 -0700

    absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - print lowercase alphabets
+ *
+ * @n: number ASCII
+ * Return: 1 lower and 0 otherwise
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
