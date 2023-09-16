b4c3b277ece607ae8c7aeefebffe16a1a60d721d
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-09-16 00:16:28 -0700

    print numbers 0 to 9 without 2 AND 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints nos without 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int no;
+
+	for (no = '0'; no <= '9'; no++)
+	{
+		if (no != '2' && no != '4')
+			_putchar(number);
+	}
+	_putchar('\n');
+}
