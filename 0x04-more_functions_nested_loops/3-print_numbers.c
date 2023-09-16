5775edaf6078a78ee3d56250f36adf1b870c7700
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-09-16 00:17:01 -0700

    print numbers 0 -9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int no;
+
+	for (no = '0'; no <= '9'; no++)
+		_putchar(number);
+	_putchar('\n');
+}
