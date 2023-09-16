788b0adee5aabbf7285c134f6f5f64caee89d982
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-09-16 00:08:38 -0700

    multiple of two nos

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two intergers
+ *
+ * @a: first int
+ * @b: second int
+ * Return: multiple of both
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
