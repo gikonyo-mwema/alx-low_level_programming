b35eca9ea7b38dc081ad2939c0103d7f073d6ac2
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-10-22 04:29:29 -0700

    string comparison

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * wildcmp - compare two srings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 similar, otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
+
