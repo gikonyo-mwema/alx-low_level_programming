3afc3ea14b325942468c8151c15e631d4750b859
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-12-21 13:52:04 -0800

    add element

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,7 +4,7 @@
  * @key: the key
  * @size: size of the array of the hash table
  *
- * Return: the index at which the key/value pair should be stored in the array of the hash table
+ * Return: the index of key/value pair  stored in the array of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
@@ -13,6 +13,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	/* Compute hash value for the key using the djb2 hash function */
 	hash_value = hash_djb2(key);
 
-	/* Compute index for the key by taking modulus of hash value with array size */
-	return hash_value % size;
+	/* Compute index for the key modulus of hash value with array size */
+	return (hash_value % size);
 }
