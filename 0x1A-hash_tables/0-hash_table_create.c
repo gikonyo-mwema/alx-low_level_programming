7a69dc202039611a0e3762ede23761b211ab0f8b
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-12-22 01:25:05 -0800

    change in malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,12 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 hash_table_t *ht;
 unsigned long int i;
 
-ht = (hash_table_t *) malloc(sizeof(hash_table_t));
+ht = malloc(sizeof(hash_table_t));
 if (ht == NULL)
 return (NULL);
 
 ht->size = size;
-ht->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
+ht->array = malloc(sizeof(hash_node_t *) * size);
 if (ht->array == NULL)
 {
 free(ht);
